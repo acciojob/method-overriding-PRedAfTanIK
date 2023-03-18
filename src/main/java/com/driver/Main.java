f3b9cbfdba792d3a50a45c7583d7e781501c2c57
PRedAfTanIK <predaftanik0@gmail.com>
2023-03-18 13:46:57 +0530

    OverRide Ass

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B objB = new B();
+        System.out.println(objB.meth());
+    }
+}
+
+class A {
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A {
+    @Override
+    public String meth() {
+        return "Method is overridden in Extendend class B";
+    }
 }
